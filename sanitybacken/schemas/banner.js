dbebee1f5aeb8b74d6f5c432ca7eebe2d92e4e39
Phongsakk <phongsak.ysd@gmail.com>
2022-09-05 10:16:22 +0700

    add sanity schemas

---
+++ sanitybacken/schemas/banner.js
@@ -0,0 +1,60 @@
+export default {
+  name: 'banner',
+  title: 'Banner',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: 'buttonText',
+      title: 'Button Text',
+      type: 'string',
+    },
+    {
+      name: 'product',
+      title: 'Product',
+      type: 'string',
+    },
+    {
+      name: 'desc',
+      title: 'Desc',
+      type: 'string',
+    },
+    {
+      name: 'smallText',
+      title: 'Small Text',
+      type: 'string',
+    },
+    {
+      name: 'midText',
+      title: 'Mid Text',
+      type: 'string',
+    },
+    {
+      name: 'largeText',
+      title: 'Large Text',
+      type: 'string',
+    },
+    {
+      name: 'largeText2',
+      title: 'Large Text 2',
+      type: 'string',
+    },
+    {
+      name: 'discount',
+      title: 'Discount',
+      type: 'string',
+    },
+    {
+      name: 'saleTime',
+      title: 'Sale Time',
+      type: 'string',
+    }
+  ]
+}
