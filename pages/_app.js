755ec10e43e46a3608a4f7428edee05e0d8f22c7
Phongsakk <phongsak.ysd@gmail.com>
2022-09-06 10:02:13 +0700

    fix layout

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { Toaster } from 'react-hot-toast'
 import { Layout } from '../components'
 import { StateContext } from '../context/StateContext'
 import '../styles/globals.css'
+import '../styles/image.css'
 
 function MyApp({ Component, pageProps }) {
   return (
